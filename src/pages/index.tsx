bcc738c66f983307697135b173c3145960c4e99a
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2026-02-08 18:18:49 +0400

    chore: update meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Материалы дисциплины"
+      description={`Учебные материалы по дисциплине «${siteConfig.title}».`}>
       <HomepageHeader />
       <main>
         <HomeAbout />
