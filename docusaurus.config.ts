a5deb960013d35eabcbfc3878c8a008e4ba1493e
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2026-02-08 19:13:57 +0400

    fix: fix edit url and remove doc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/viacheslav-dobrynin/structures-and-algorithms-in-databases-and-distributed-systems/tree/main/',
         },
         blog: false,
         theme: {
